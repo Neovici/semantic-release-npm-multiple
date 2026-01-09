3acc545ef7fbb555bc268eb243b5d69b8e157fdd
basilgood <elsile69@yahoo.com>
2026-01-09 23:12:36 +0200

    chore(deps-dev): Bump husky from 4.x to 9.1.7.

--- husky.config.cjs
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  hooks: {
-    'commit-msg': 'commitlint -E HUSKY_GIT_PARAMS',
-    'pre-commit':
-      'lint-staged && NODE_OPTIONS=--experimental-vm-modules jest --passWithNoTests -o',
-  },
-};
